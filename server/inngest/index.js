b5f730583ac95b69d38e0a2adadbf27f4a801e0f
AbdullahAlZubaerOfficial <zubaerislam703@gmail.com>
2025-11-18 18:39:57 +0600

    up

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,16 +9,22 @@ const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk' },
     { event: 'user.created' },
     async ({ event }) => {
-        const { data } = event;
-        await prisma.user.create({
-            data: {
-                id: data.id,
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        });
-    }
+  const { data } = event;
+  try {
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      }
+    });
+    console.log("User saved:", data.id);
+  } catch (err) {
+    console.error("Prisma error:", err);
+  }
+}
+
 );
 
 // Inngest Function to delete user data from database
