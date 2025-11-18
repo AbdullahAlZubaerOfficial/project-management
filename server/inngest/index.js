a55ee72df2fe1c940dfee65916d647259a02933f
AbdullahAlZubaerOfficial <zubaerislam703@gmail.com>
2025-11-18 17:24:44 +0600

    up

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,60 +6,52 @@ export const inngest = new Inngest({ id: "Project-Managementtttt" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id:'sync-user-from-clerk'},
-    {event:'clerk/user.created'},
-    async({event})=>{
-        const {data} = event;
+    { id: 'sync-user-from-clerk' },
+    { event: 'user.created' },
+    async ({ event }) => {
+        const { data } = event;
         await prisma.user.create({
             data: {
                 id: data.id,
                 email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
-                image:data?.image_url,
+                image: data?.image_url,
             }
-        })
+        });
     }
-)
-
+);
 
 // Inngest Function to delete user data from database
 const syncUserDeletion = inngest.createFunction(
-    {id:'sync-user-with-clerk'},
-    {event:'clerk/user.deleted'},
-    async({event})=>{
-        const {data} = event;
+    { id: 'sync-user-with-clerk' },
+    { event: 'user.deleted' },       // ✅ FIXED
+    async ({ event }) => {
+        const { data } = event;
         await prisma.user.delete({
-            where: {
-                id: data.id,
-            }
-        })
+            where: { id: data.id }
+        });
     }
-)
-
+);
 
 // Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-    {id:'update-user-from-clerk'},
-    {event:'clerk/user.updated'},
-    async({event})=>{
-        const {data} = event;
+    { id: 'update-user-from-clerk' },
+    { event: 'user.updated' },      // ✅ FIXED
+    async ({ event }) => {
+        const { data } = event;
         await prisma.user.update({
-            where: {
-                id: data.id,
-            },
+            where: { id: data.id },
             data: {
                 id: data.id,
                 email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
-                image:data?.image_url,
+                image: data?.image_url,
             }
-        })
+        });
     }
-)
+);
 
-
-
-// Create an empty array where we'll export future Inngest functions
+// Export functions
 export const functions = [
     syncUserCreation,
     syncUserDeletion,
